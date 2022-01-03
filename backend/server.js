83337da864aefbbf036467944bab4f4beb8fe744
Vllaadd <vlladdiimiir@gmail.com>
2022-01-02 23:01:55 -0500

    app is now connected to the database

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,8 @@ app.use(express.json());
 // MONGO CONNECTION ----------------------------------------------------
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { 
-  useNewUrlParser: true, 
-  useCreateIndex: true 
+  useNewUrlParser: true 
+  // useCreateIndex: true 
 });
 const connection = mongoose.connection;
 connection.once('open', () => {
